608443aaa0eea68b0e5c2a672f8ff65f78bf7890
YvanTlx <31309002+YvanTlx@users.noreply.github.com>
2024-03-30 23:44:46 +0200

    Update HelloApplication.java

--- src/main/java/org/yves/buzzfuzz/HelloApplication.java
+++ src/main/java/org/yves/buzzfuzz/HelloApplication.java
@@ -8,10 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class HelloApplication extends Application {
+public class FuzzBuzzExercise extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FuzzBuzzExercise.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
