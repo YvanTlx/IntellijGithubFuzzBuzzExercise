a76230e92d294668cf627706319bf985eaba114b
YvanTlx <31309002+YvanTlx@users.noreply.github.com>
2024-03-30 23:22:34 +0200

    Fuzz Buzz Exercise

---
+++ src/main/java/org/yves/buzzfuzz/HelloApplication.java
@@ -0,0 +1,36 @@
+package org.yves.buzzfuzz;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number=");
+        int number = scanner.nextInt();
+        if (number % 5 == 0){
+            if (number % 3 == 0)
+                System.out. println("FIZZBUZZ");
+            else
+                System.out.println("FIZZ");
+        }
+        else if(number % 3 == 0)
+            System.out.print("BUZZ");
+        else
+            System.out.print(number);
+    }
+}
